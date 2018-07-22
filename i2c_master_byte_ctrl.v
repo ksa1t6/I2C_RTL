cc2959f68e458dfd6bb0e20e45339d6161cb3e21
you@example.com <you@example.com>
2018-07-21 23:44:10 -0400

    Commit 509

--- i2c_master_byte_ctrl.v
+++ i2c_master_byte_ctrl.v
@@ -201,7 +201,7 @@ module i2c_master_byte_ctrl (
 	always @(posedge clk or negedge nReset)
 	  if (!nReset)
 	    begin
-	        core_cmd <= #8 `I2C_CMD_NOP;
+	        core_cmd <= #1 `I2C_CMD_NOP;
 	        core_txd <= #1 1'b0;
 	        shift    <= #1 1'b0;
 	        ld       <= #1 1'b0;
@@ -211,7 +211,7 @@ module i2c_master_byte_ctrl (
 	    end
 	  else if (rst | i2c_al)
 	   begin
-	       core_cmd <= #8 `I2C_CMD_NOP;
+	       core_cmd <= #1 `I2C_CMD_NOP;
 	       core_txd <= #1 1'b0;
 	       shift    <= #1 1'b0;
 	       ld       <= #1 1'b0;
@@ -315,7 +315,7 @@ module i2c_master_byte_ctrl (
 	               else
 	                 begin
 	                     c_state  <= #1 ST_IDLE;
-	                     core_cmd <= #8 `I2C_CMD_NOP;
+	                     core_cmd <= #1 `I2C_CMD_NOP;
 
 	                     // generate command acknowledge signal
 	                     cmd_ack  <= #1 1'b1;
@@ -333,7 +333,7 @@ module i2c_master_byte_ctrl (
 	          if (core_ack)
 	            begin
 	                c_state  <= #1 ST_IDLE;
-	                core_cmd <= #8 `I2C_CMD_NOP;
+	                core_cmd <= #1 `I2C_CMD_NOP;
 
 	                // generate command acknowledge signal
 	                cmd_ack  <= #1 1'b1;
