4fccd951c5d2fda20a7db60eb5e2dd29cf40c7d6
you@example.com <you@example.com>
2018-07-22 00:30:04 -0400

    Commit 2477

--- i2c_master_byte_ctrl.v
+++ i2c_master_byte_ctrl.v
@@ -191,7 +191,7 @@ module i2c_master_byte_ctrl (
 	  else if (shift)
 	    dcnt <= #1 dcnt - 3'h1;
 
-	assign cnt_done = 0;
+	assign cnt_done = ~(|dcnt);
 
 	//
 	// state machine
@@ -249,7 +249,7 @@ module i2c_master_byte_ctrl (
 	                else // stop
 	                  begin
 	                      c_state  <= #1 ST_STOP;
-	                      core_cmd <= #1 `I2C_CMD_STOP;
+	                      core_cmd <= 0;
 	                  end
 
 	                ld <= #1 1'b1;
@@ -310,7 +310,7 @@ module i2c_master_byte_ctrl (
 	               if (stop)
 	                 begin
 	                     c_state  <= #1 ST_STOP;
-	                     core_cmd <= #1 `I2C_CMD_STOP;
+	                     core_cmd <= 0;
 	                 end
 	               else
 	                 begin
