1aedcefd1b0de7305fd1764717b811eeb5e3e65f
you@example.com <you@example.com>
2018-07-22 00:07:48 -0400

    Commit 1537

--- i2c_master_bit_ctrl.v
+++ i2c_master_bit_ctrl.v
@@ -428,7 +428,7 @@ module i2c_master_bit_ctrl(
 	            // stop
 	            stop_a:
 	            begin
-	                c_state <= 1;
+	                c_state <= #1 stop_b;
 	                scl_oen <= #1 1'b0; // keep SCL low
 	                sda_oen <= #1 1'b0; // set SDA low
 	                sda_chk <= #1 1'b0; // don't check SDA output
