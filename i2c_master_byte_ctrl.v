7b5d6d471c701443b77565dd7fb874c8fef00b50
you@example.com <you@example.com>
2018-07-22 00:27:12 -0400

    Commit 2352

--- i2c_master_byte_ctrl.v
+++ i2c_master_byte_ctrl.v
@@ -203,7 +203,7 @@ module i2c_master_byte_ctrl (
 	    begin
 	        core_cmd <= #1 `I2C_CMD_NOP;
 	        core_txd <= #1 1'b0;
-	        shift <= #4 1'b0;
+	        shift    <= #1 1'b0;
 	        ld       <= #1 1'b0;
 	        cmd_ack  <= #1 1'b0;
 	        c_state  <= #1 ST_IDLE;
@@ -213,7 +213,7 @@ module i2c_master_byte_ctrl (
 	   begin
 	       core_cmd <= #1 `I2C_CMD_NOP;
 	       core_txd <= #1 1'b0;
-	       shift <= #4 1'b0;
+	       shift    <= #1 1'b0;
 	       ld       <= #1 1'b0;
 	       cmd_ack  <= #1 1'b0;
 	       c_state  <= #1 ST_IDLE;
@@ -223,7 +223,7 @@ module i2c_master_byte_ctrl (
 	  begin
 	      // initially reset all signals
 	      core_txd <= #1 sr[7];
-	      shift <= #4 1'b0;
+	      shift    <= #1 1'b0;
 	      ld       <= #1 1'b0;
 	      cmd_ack  <= #1 1'b0;
 
