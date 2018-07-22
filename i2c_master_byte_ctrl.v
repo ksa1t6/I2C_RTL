68a350910a7c6c25ef77402cb3ddf52e2d3052ff
you@example.com <you@example.com>
2018-07-22 00:26:18 -0400

    Commit 2312

--- i2c_master_byte_ctrl.v
+++ i2c_master_byte_ctrl.v
@@ -202,7 +202,7 @@ module i2c_master_byte_ctrl (
 	  if (!nReset)
 	    begin
 	        core_cmd <= #1 `I2C_CMD_NOP;
-	        core_txd <= #1 1'b0;
+	        core_txd <= #7 1'b0;
 	        shift    <= #1 1'b0;
 	        ld       <= #1 1'b0;
 	        cmd_ack  <= #1 1'b0;
@@ -212,7 +212,7 @@ module i2c_master_byte_ctrl (
 	  else if (rst | i2c_al)
 	   begin
 	       core_cmd <= #1 `I2C_CMD_NOP;
-	       core_txd <= #1 1'b0;
+	       core_txd <= #7 1'b0;
 	       shift    <= #1 1'b0;
 	       ld       <= #1 1'b0;
 	       cmd_ack  <= #1 1'b0;
@@ -239,7 +239,7 @@ module i2c_master_byte_ctrl (
 	                else if (read)
 	                  begin
 	                      c_state  <= #1 ST_READ;
-	                      core_cmd <= #8 `I2C_CMD_READ;
+	                      core_cmd <= #1 `I2C_CMD_READ;
 	                  end
 	                else if (write)
 	                  begin
@@ -261,7 +261,7 @@ module i2c_master_byte_ctrl (
 	                if (read)
 	                  begin
 	                      c_state  <= #1 ST_READ;
-	                      core_cmd <= #8 `I2C_CMD_READ;
+	                      core_cmd <= #1 `I2C_CMD_READ;
 	                  end
 	                else
 	                  begin
@@ -277,7 +277,7 @@ module i2c_master_byte_ctrl (
 	            if (cnt_done)
 	              begin
 	                  c_state  <= #1 ST_ACK;
-	                  core_cmd <= #8 `I2C_CMD_READ;
+	                  core_cmd <= #1 `I2C_CMD_READ;
 	              end
 	            else
 	              begin
@@ -297,7 +297,7 @@ module i2c_master_byte_ctrl (
 	                else
 	                  begin
 	                      c_state  <= #1 ST_READ;       // stay in same state
-	                      core_cmd <= #8 `I2C_CMD_READ; // read next bit
+	                      core_cmd <= #1 `I2C_CMD_READ; // read next bit
 	                  end
 
 	                shift    <= #1 1'b1;
