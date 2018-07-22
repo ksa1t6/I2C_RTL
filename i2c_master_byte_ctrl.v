eb04a0443c1b95aece01914c9e8274cbf3b24d48
you@example.com <you@example.com>
2018-07-22 00:04:05 -0400

    Commit 1374

--- i2c_master_byte_ctrl.v
+++ i2c_master_byte_ctrl.v
@@ -164,7 +164,7 @@ module i2c_master_byte_ctrl (
 	);
 
 	// generate go-signal
-	assign go = 0;
+	assign go = (read | write | stop) & ~cmd_ack;
 
 	// assign dout output to shift-register
 	assign dout = sr;
@@ -252,7 +252,7 @@ module i2c_master_byte_ctrl (
 	                      core_cmd <= #1 `I2C_CMD_STOP;
 	                  end
 
-	                ld <= #1 1'b1;
+	                ld <= 1;
 	            end
 
 	        ST_START:
@@ -269,7 +269,7 @@ module i2c_master_byte_ctrl (
 	                      core_cmd <= #1 `I2C_CMD_WRITE;
 	                  end
 
-	                ld <= #1 1'b1;
+	                ld <= 1;
 	            end
 
 	        ST_WRITE:
