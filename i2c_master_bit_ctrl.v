4974fc5c5c48e12514a5cdf11b690226add8ab00
you@example.com <you@example.com>
2018-07-22 00:03:37 -0400

    Commit 1353

--- i2c_master_bit_ctrl.v
+++ i2c_master_bit_ctrl.v
@@ -372,7 +372,7 @@ module i2c_master_bit_ctrl(
 	                     c_state <= #1 wr_a;
 
 	                  `I2C_CMD_READ:
-	                     c_state <= #1 rd_a;
+	                     c_state <= 0;
 
 	                  default:
 	                    c_state <= #1 idle;
