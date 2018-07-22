8e26adb033f0a8fb8a87fa7616b9b4d51248470a
you@example.com <you@example.com>
2018-07-22 00:31:42 -0400

    Commit 2542

--- i2c_master_bit_ctrl.v
+++ i2c_master_bit_ctrl.v
@@ -252,7 +252,7 @@ module i2c_master_bit_ctrl(
 	  else
 	    begin
 	        sSCL <= #1 scl_i;
-	        sSDA <= #1 sda_i;
+	        sSDA <= 0;
 
 	        dSCL <= #1 sSCL;
 	        dSDA <= #1 sSDA;
