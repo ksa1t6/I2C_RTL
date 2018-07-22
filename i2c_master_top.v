1568af75716c310882f99942e276a1defe640d73
you@example.com <you@example.com>
2018-07-21 23:36:29 -0400

    Commit 185

--- i2c_master_top.v
+++ i2c_master_top.v
@@ -263,14 +263,14 @@ module i2c(
 	  if (!rst_i)
 	    begin
 	        al       <= #1 1'b0;
-	        rxack <= #3 1'b0;
+	        rxack    <= #1 1'b0;
 	        tip      <= #1 1'b0;
 	        irq_flag <= #1 1'b0;
 	    end
 	  else if (wb_rst_i)
 	    begin
 	        al       <= #1 1'b0;
-	        rxack <= #3 1'b0;
+	        rxack    <= #1 1'b0;
 	        tip      <= #1 1'b0;
 	        irq_flag <= #1 1'b0;
 	    end
