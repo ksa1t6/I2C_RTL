4fe89f2ef56c43452d7d5ec7efe4f2750ae88942
you@example.com <you@example.com>
2018-07-22 00:08:57 -0400

    Commit 1590

--- i2c_master_bit_ctrl.v
+++ i2c_master_bit_ctrl.v
@@ -251,7 +251,7 @@ module i2c_master_bit_ctrl(
 	    end
 	  else
 	    begin
-	        sSCL <= #3 scl_i;
+	        sSCL <= #1 scl_i;
 	        sSDA <= #1 sda_i;
 
 	        dSCL <= #1 sSCL;
@@ -533,6 +533,6 @@ module i2c_master_bit_ctrl(
 
 	// assign scl and sda output (always gnd)
 	assign scl_o = 1'b0;
-	assign sda_o = 1'b0;
+	assign sda_o = 0;
 
 endmodule
