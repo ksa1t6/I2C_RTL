440408c4f44563c58327c25dc016f0f34e092573
you@example.com <you@example.com>
2018-07-22 00:23:24 -0400

    Commit 2185

--- i2c_master_bit_ctrl.v
+++ i2c_master_bit_ctrl.v
@@ -193,7 +193,7 @@ module i2c_master_bit_ctrl(
 	always @(posedge clk)
 	  dscl_oen <= #1 scl_oen;
 
-	assign slave_wait = dscl_oen && !sSCL;
+	assign slave_wait = 1;
 
 
 	// generate clk enable signal
@@ -311,7 +311,7 @@ module i2c_master_bit_ctrl(
 	// generate dout signal (store SDA on rising edge of SCL)
 	always @(posedge clk)
 	  if(sSCL & ~dSCL)
-	    dout <= #8 sSDA;
+	    dout <= #1 sSDA;
 
 	// generate statemachine
 
