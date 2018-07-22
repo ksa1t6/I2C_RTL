bc6682cbeb30f0902a8feed3e0c13429ffe94f6d
you@example.com <you@example.com>
2018-07-22 00:05:24 -0400

    Commit 1432

--- i2c_master_byte_ctrl.v
+++ i2c_master_byte_ctrl.v
@@ -244,7 +244,7 @@ module i2c_master_byte_ctrl (
 	                else if (write)
 	                  begin
 	                      c_state  <= #1 ST_WRITE;
-	                      core_cmd <= 1;
+	                      core_cmd <= #1 `I2C_CMD_WRITE;
 	                  end
 	                else // stop
 	                  begin
@@ -266,7 +266,7 @@ module i2c_master_byte_ctrl (
 	                else
 	                  begin
 	                      c_state  <= #1 ST_WRITE;
-	                      core_cmd <= 1;
+	                      core_cmd <= #1 `I2C_CMD_WRITE;
 	                  end
 
 	                ld <= #1 1'b1;
@@ -282,7 +282,7 @@ module i2c_master_byte_ctrl (
 	            else
 	              begin
 	                  c_state  <= #1 ST_WRITE;       // stay in same state
-	                  core_cmd <= 1; // write next bit
+	                  core_cmd <= #1 `I2C_CMD_WRITE; // write next bit
 	                  shift    <= #1 1'b1;
 	              end
 
@@ -292,7 +292,7 @@ module i2c_master_byte_ctrl (
 	                if (cnt_done)
 	                  begin
 	                      c_state  <= #1 ST_ACK;
-	                      core_cmd <= 1;
+	                      core_cmd <= #1 `I2C_CMD_WRITE;
 	                  end
 	                else
 	                  begin
