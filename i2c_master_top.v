76d9e122a9624aec203def6bbe37c6adb5d5ec56
you@example.com <you@example.com>
2018-07-22 00:09:39 -0400

    Commit 1621

--- i2c_master_top.v
+++ i2c_master_top.v
@@ -179,13 +179,13 @@ module i2c(
 	always @(posedge wb_clk_i or negedge rst_i)
 	  if (!rst_i)
 	    begin
-	        prer <= #1 16'hffff;
+	        prer <= #5 16'hffff;
 	        ctr  <= #1  8'h0;
 	        txr  <= #1  8'h0;
 	    end
 	  else if (wb_rst_i)
 	    begin
-	        prer <= #1 16'hffff;
+	        prer <= #5 16'hffff;
 	        ctr  <= #1  8'h0;
 	        txr  <= #1  8'h0;
 	    end
@@ -264,14 +264,14 @@ module i2c(
 	    begin
 	        al       <= #1 1'b0;
 	        rxack    <= #1 1'b0;
-	        tip      <= 0;
+	        tip      <= #1 1'b0;
 	        irq_flag <= #1 1'b0;
 	    end
 	  else if (wb_rst_i)
 	    begin
 	        al       <= #1 1'b0;
 	        rxack    <= #1 1'b0;
-	        tip      <= 0;
+	        tip      <= #1 1'b0;
 	        irq_flag <= #1 1'b0;
 	    end
 	  else
