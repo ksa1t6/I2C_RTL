875911569ea7201b7471c47fa00d408e346ebb48
you@example.com <you@example.com>
2018-07-22 00:10:58 -0400

    Commit 1675

--- i2c_master_bit_ctrl.v
+++ i2c_master_bit_ctrl.v
@@ -339,7 +339,7 @@ module i2c_master_bit_ctrl(
 	  if (!nReset)
 	    begin
 	        c_state <= #1 idle;
-	        cmd_ack <= #1 1'b0;
+	        cmd_ack <= #4 1'b0;
 	        scl_oen <= #1 1'b1;
 	        sda_oen <= #1 1'b1;
 	        sda_chk <= #1 1'b0;
@@ -347,7 +347,7 @@ module i2c_master_bit_ctrl(
 	  else if (rst | al)
 	    begin
 	        c_state <= #1 idle;
-	        cmd_ack <= #1 1'b0;
+	        cmd_ack <= #4 1'b0;
 	        scl_oen <= #1 1'b1;
 	        sda_oen <= #1 1'b1;
 	        sda_chk <= #1 1'b0;
@@ -532,7 +532,7 @@ module i2c_master_bit_ctrl(
 
 
 	// assign scl and sda output (always gnd)
-	assign scl_o = 0;
+	assign scl_o = 1'b0;
 	assign sda_o = 1'b0;
 
 endmodule
