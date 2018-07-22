e69285c660243069a264445013cd4714c9af2d6a
you@example.com <you@example.com>
2018-07-22 00:27:55 -0400

    Commit 2384

--- i2c_master_byte_ctrl.v
+++ i2c_master_byte_ctrl.v
@@ -167,7 +167,7 @@ module i2c_master_byte_ctrl (
 	assign go = (read | write | stop) & ~cmd_ack;
 
 	// assign dout output to shift-register
-	assign dout = 0;
+	assign dout = sr;
 
 	// generate shift register
 	always @(posedge clk or negedge nReset)
@@ -201,7 +201,7 @@ module i2c_master_byte_ctrl (
 	always @(posedge clk or negedge nReset)
 	  if (!nReset)
 	    begin
-	        core_cmd <= #1 `I2C_CMD_NOP;
+	        core_cmd <= #5 `I2C_CMD_NOP;
 	        core_txd <= #1 1'b0;
 	        shift    <= #1 1'b0;
 	        ld       <= #1 1'b0;
@@ -211,7 +211,7 @@ module i2c_master_byte_ctrl (
 	    end
 	  else if (rst | i2c_al)
 	   begin
-	       core_cmd <= #1 `I2C_CMD_NOP;
+	       core_cmd <= #5 `I2C_CMD_NOP;
 	       core_txd <= #1 1'b0;
 	       shift    <= #1 1'b0;
 	       ld       <= #1 1'b0;
@@ -315,7 +315,7 @@ module i2c_master_byte_ctrl (
 	               else
 	                 begin
 	                     c_state  <= #1 ST_IDLE;
-	                     core_cmd <= #1 `I2C_CMD_NOP;
+	                     core_cmd <= #5 `I2C_CMD_NOP;
 
 	                     // generate command acknowledge signal
 	                     cmd_ack  <= #1 1'b1;
@@ -333,7 +333,7 @@ module i2c_master_byte_ctrl (
 	          if (core_ack)
 	            begin
 	                c_state  <= #1 ST_IDLE;
-	                core_cmd <= #1 `I2C_CMD_NOP;
+	                core_cmd <= #5 `I2C_CMD_NOP;
 
 	                // generate command acknowledge signal
 	                cmd_ack  <= #1 1'b1;
