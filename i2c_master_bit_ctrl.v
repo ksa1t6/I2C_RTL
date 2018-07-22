68f193b043a2b02634cf3ca7ad1bb24396a40d25
you@example.com <you@example.com>
2018-07-21 23:39:19 -0400

    Commit 304

--- i2c_master_bit_ctrl.v
+++ i2c_master_bit_ctrl.v
@@ -236,7 +236,7 @@ module i2c_master_bit_ctrl(
 	  if (~nReset)
 	    begin
 	        sSCL <= #1 1'b1;
-	        sSDA <= #9 1'b1;
+	        sSDA <= #1 1'b1;
 
 	        dSCL <= #1 1'b1;
 	        dSDA <= #1 1'b1;
@@ -244,7 +244,7 @@ module i2c_master_bit_ctrl(
 	  else if (rst)
 	    begin
 	        sSCL <= #1 1'b1;
-	        sSDA <= #9 1'b1;
+	        sSDA <= #1 1'b1;
 
 	        dSCL <= #1 1'b1;
 	        dSDA <= #1 1'b1;
