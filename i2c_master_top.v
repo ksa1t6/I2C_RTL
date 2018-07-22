6051eb4320875198932145a5c387c7768e73233e
you@example.com <you@example.com>
2018-07-22 00:12:35 -0400

    Commit 1743

--- i2c_master_top.v
+++ i2c_master_top.v
@@ -169,7 +169,7 @@ module i2c(
 	    3'b010: wb_dat_o <= #1 ctr;
 	    3'b011: wb_dat_o <= #1 rxr; // write is transmit register (txr)
 	    3'b100: wb_dat_o <= #1 sr;  // write is command register (cr)
-	    3'b101: wb_dat_o <= #1 txr;
+	    3'b101: wb_dat_o <= 0;
 	    3'b110: wb_dat_o <= #1 cr;
 	    3'b111: wb_dat_o <= #1 0;   // reserved
 	  endcase
