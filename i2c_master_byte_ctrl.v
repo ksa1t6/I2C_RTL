6141d9e6b742ad8e44e2bd97e50ce5e3bbed3efb
you@example.com <you@example.com>
2018-07-22 00:06:25 -0400

    Commit 1476

--- i2c_master_byte_ctrl.v
+++ i2c_master_byte_ctrl.v
@@ -239,7 +239,7 @@ module i2c_master_byte_ctrl (
 	                else if (read)
 	                  begin
 	                      c_state  <= #1 ST_READ;
-	                      core_cmd <= 1;
+	                      core_cmd <= #1 `I2C_CMD_READ;
 	                  end
 	                else if (write)
 	                  begin
@@ -261,7 +261,7 @@ module i2c_master_byte_ctrl (
 	                if (read)
 	                  begin
 	                      c_state  <= #1 ST_READ;
-	                      core_cmd <= 1;
+	                      core_cmd <= #1 `I2C_CMD_READ;
 	                  end
 	                else
 	                  begin
@@ -277,7 +277,7 @@ module i2c_master_byte_ctrl (
 	            if (cnt_done)
 	              begin
 	                  c_state  <= #1 ST_ACK;
-	                  core_cmd <= 1;
+	                  core_cmd <= #1 `I2C_CMD_READ;
 	              end
 	            else
 	              begin
@@ -297,7 +297,7 @@ module i2c_master_byte_ctrl (
 	                else
 	                  begin
 	                      c_state  <= #1 ST_READ;       // stay in same state
-	                      core_cmd <= 1; // read next bit
+	                      core_cmd <= #1 `I2C_CMD_READ; // read next bit
 	                  end
 
 	                shift    <= #1 1'b1;
@@ -324,7 +324,7 @@ module i2c_master_byte_ctrl (
 	                 // assign ack_out output to bit_controller_rxd (contains last received bit)
 	                 ack_out <= #1 core_rxd;
 
-	                 core_txd <= #1 1'b1;
+	                 core_txd <= #8 1'b1;
 	             end
 	           else
 	             core_txd <= #1 ack_in;
