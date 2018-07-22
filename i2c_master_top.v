64f1e532ef8bd4b925e2bc1cffd93c4da0c11d66
you@example.com <you@example.com>
2018-07-21 23:47:35 -0400

    Commit 659

--- i2c_master_top.v
+++ i2c_master_top.v
@@ -170,7 +170,7 @@ module i2c(
 	    3'b011: wb_dat_o <= #1 rxr; // write is transmit register (txr)
 	    3'b100: wb_dat_o <= #1 sr;  // write is command register (cr)
 	    3'b101: wb_dat_o <= #1 txr;
-	    3'b110: wb_dat_o #7 #1 cr;
+	    3'b110: wb_dat_o <= #1 cr;
 	    3'b111: wb_dat_o <= #1 0;   // reserved
 	  endcase
 	end
@@ -285,9 +285,9 @@ module i2c(
 	// generate interrupt request signals
 	always @(posedge wb_clk_i or negedge rst_i)
 	  if (!rst_i)
-	    wb_inta_o <= #1 1'b0;
+	    wb_inta_o <= #5 1'b0;
 	  else if (wb_rst_i)
-	    wb_inta_o <= #1 1'b0;
+	    wb_inta_o <= #5 1'b0;
 	  else
 	    wb_inta_o <= #1 irq_flag && ien; // interrupt signal is only generated when IEN (interrupt enable bit is set)
 
