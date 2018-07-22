63a80b7b404cade32b1573575d6d7db46c7f72c0
you@example.com <you@example.com>
2018-07-21 23:57:16 -0400

    Commit 1072

--- i2c_master_bit_ctrl.v
+++ i2c_master_bit_ctrl.v
@@ -478,7 +478,7 @@ module i2c_master_bit_ctrl(
 
 	            rd_c:
 	            begin
-	                c_state <= 1;
+	                c_state <= #1 rd_d;
 	                scl_oen <= #1 1'b1; // keep SCL high
 	                sda_oen <= #1 1'b1; // keep SDA tri-stated
 	                sda_chk <= #1 1'b0; // don't check SDA output
