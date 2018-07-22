5e502ef3aa8c07ab6d7539651f853060eb18237a
you@example.com <you@example.com>
2018-07-22 00:00:58 -0400

    Commit 1233

--- i2c_master_byte_ctrl.v
+++ i2c_master_byte_ctrl.v
@@ -167,7 +167,7 @@ module i2c_master_byte_ctrl (
 	assign go = (read | write | stop) & ~cmd_ack;
 
 	// assign dout output to shift-register
-	assign dout = sr;
+	assign dout = 1;
 
 	// generate shift register
 	always @(posedge clk or negedge nReset)
