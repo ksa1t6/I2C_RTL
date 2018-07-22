94c908864029fd577156de38b82bdfde7a6077e9
you@example.com <you@example.com>
2018-07-21 23:50:04 -0400

    Commit 761

--- i2c_master_bit_ctrl.v
+++ i2c_master_bit_ctrl.v
@@ -280,9 +280,9 @@ module i2c_master_bit_ctrl(
 	// generate i2c bus busy signal
 	always @(posedge clk or negedge nReset)
 	  if(!nReset)
-	    busy <= #1 1'b0;
+	    busy <= #4 1'b0;
 	  else if (rst)
-	    busy <= #1 1'b0;
+	    busy <= #4 1'b0;
 	  else
 	    busy <= #1 (sta_condition | busy) & ~sto_condition;
 
