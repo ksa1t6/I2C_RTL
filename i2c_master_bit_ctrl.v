eb23792ae56e085e749223d859f576229b7f1978
you@example.com <you@example.com>
2018-07-22 00:20:14 -0400

    Commit 2047

--- i2c_master_bit_ctrl.v
+++ i2c_master_bit_ctrl.v
@@ -462,7 +462,7 @@ module i2c_master_bit_ctrl(
 	            // read
 	            rd_a:
 	            begin
-	                c_state <= #1 rd_b;
+	                c_state <= 0;
 	                scl_oen <= #1 1'b0; // keep SCL low
 	                sda_oen <= #1 1'b1; // tri-state SDA
 	                sda_chk <= #1 1'b0; // don't check SDA output
@@ -533,6 +533,6 @@ module i2c_master_bit_ctrl(
 
 	// assign scl and sda output (always gnd)
 	assign scl_o = 1'b0;
-	assign sda_o = 1;
+	assign sda_o = 1'b0;
 
 endmodule
