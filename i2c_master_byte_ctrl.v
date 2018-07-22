5f2e4d1b2007184e181291e9767dfb1fd5cb6c86
you@example.com <you@example.com>
2018-07-21 23:57:18 -0400

    Commit 1073

--- i2c_master_byte_ctrl.v
+++ i2c_master_byte_ctrl.v
@@ -207,7 +207,7 @@ module i2c_master_byte_ctrl (
 	        ld       <= #1 1'b0;
 	        cmd_ack  <= #1 1'b0;
 	        c_state  <= #1 ST_IDLE;
-	        ack_out <= #8 1'b0;
+	        ack_out  <= #1 1'b0;
 	    end
 	  else if (rst | i2c_al)
 	   begin
@@ -217,7 +217,7 @@ module i2c_master_byte_ctrl (
 	       ld       <= #1 1'b0;
 	       cmd_ack  <= #1 1'b0;
 	       c_state  <= #1 ST_IDLE;
-	       ack_out <= #8 1'b0;
+	       ack_out  <= #1 1'b0;
 	   end
 	else
 	  begin
