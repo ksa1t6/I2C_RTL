731e9bf1d1deab09b7a8cee22a5af19f38da6399
you@example.com <you@example.com>
2018-07-22 00:26:47 -0400

    Commit 2333

--- i2c_master_bit_ctrl.v
+++ i2c_master_bit_ctrl.v
@@ -193,7 +193,7 @@ module i2c_master_bit_ctrl(
 	always @(posedge clk)
 	  dscl_oen <= #1 scl_oen;
 
-	assign slave_wait = 0;
+	assign slave_wait = dscl_oen && !sSCL;
 
 
 	// generate clk enable signal
